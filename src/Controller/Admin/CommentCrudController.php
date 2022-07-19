a0ef549342e141d08a792ed213397b435e633313
Melo-dev16 <melcedricdatcha@gmail.com>
2022-07-19 10:26:08 +0200

    Show conference pag added

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -6,7 +6,7 @@ use App\Entity\Comment;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextAreaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 
 class CommentCrudController extends AbstractCrudController
@@ -24,7 +24,7 @@ class CommentCrudController extends AbstractCrudController
             TextField::new('email', 'Votre Email'),
             DateTimeField::new('createdAt', 'Créé le'),
             AssociationField::new('conference', 'Conférence')->autocomplete(),
-            TextEditorField::new('text', 'Commentaire'),
+            TextAreaField::new('text', 'Commentaire'),
         ];
     }
     
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,23 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'Conference')]
-    public function index(Request $request): Response
+    public function index(Environment $twig, ConferenceRepository $repo): Response
     {
-        $greet = '';
+       
+        return new Response($twig->render('conference/index.html.twig',
+            [
+                'conferences' => $repo->findAll()
+            ]
+        ));
+    }
 
-        if($name = $request->query->get('hello')){
-            $greet = sprintf('Hello %s', htmlspecialchars($name));
-        }
-        return $this->render('conference/index.html.twig', [
-            'greet' => $greet
-        ]);
+    #[Route('/conference/{id}', name: 'SingleConference')]
+    public function show(Environment $twig, Conference $conference, CommentRepository $comRepo): Response
+    {
+       
+        return new Response($twig->render('conference/show.html.twig',
+            [
+                'conference' => $conference,
+                'comments' => $comRepo->findBy(['conference' => $conference],['createdAt' => 'DESC'])
+            ]
+        ));
     }
 }
